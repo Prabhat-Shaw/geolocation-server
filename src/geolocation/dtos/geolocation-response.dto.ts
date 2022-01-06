4578fcbdb08942040027debb8cc15a42cf63b8a0
pietrzakadrian <contact.pietrzakadrian@gmail.com>
2022-01-06 15:00:36 +0100

    feat(): upgrade dto

--- src/geolocation/dtos/geolocation-response.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IntersectionType, PartialType } from '@nestjs/swagger';
-import { ErrorResponseDto, GeolocationDto } from '.';
-
-export class GeolocationResponseDto extends PartialType(
-  IntersectionType(GeolocationDto, ErrorResponseDto),
-) {}
